51131d1aec144c015208561efef7bef44566cb01
Kristopher Schrieken <krisschrieken@gmail.com>
2019-09-28 15:08:45 -0700

    Initial Commit

---
+++ CalCounter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CalCounter
+//
+//  Created by Kristopher Schrieken on 9/28/19.
+//  Copyright © 2019 Kristopher Schrieken. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
